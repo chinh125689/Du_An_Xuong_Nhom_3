1d6c7766d6bc011cbd31727353839814f7ec17ce
dungngduy <148855343+dungngduy@users.noreply.github.com>
2023-11-05 23:47:50 +0700

    update details grid

---
+++ Website_hotroSV/main.js
@@ -0,0 +1,13 @@
+// $('#menu > li').hover(function() {
+//     // khi thẻ menu li bị hover thì drop down menu thuộc thẻ li đó sẽ trượt xuống(hiện)
+//     $('.dropdown', this).slideDown();
+// },function() {
+//     // khi thẻ menu li bị out không hover nữa thì drop down menu thuộc thẻ li đó sẽ trượt lên(ẩn)
+//     $('.dropdown', this).slideUp();
+// });
+
+$('.dropdown > a').click(function() {
+    $(this).parent().siblings().find('ul').fadeOut('500');
+    $(this).next('ul').stop(true, false, true).fadeToggle('500');
+    return false;
+});
