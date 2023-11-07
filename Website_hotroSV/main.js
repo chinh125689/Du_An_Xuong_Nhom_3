ffc3ec4e716aa05ad5efd9b819d310c1a1d7ae71
dungngduy <148855343+dungngduy@users.noreply.github.com>
2023-11-07 14:47:19 +0700

    update sign in

--- Website_hotroSV/main.js
+++ Website_hotroSV/main.js
@@ -7,7 +7,7 @@
 // });
 
 $('.dropdown > a').click(function() {
-    $(this).parent().siblings().find('ul').fadeOut('500');
-    $(this).next('ul').stop(true, false, true).fadeToggle('500');
+    $(this).parent().siblings().find('div').fadeOut('500');
+    $(this).next('div').stop(true, false, true).fadeToggle('500');
     return false;
 });
