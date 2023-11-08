a3671b33d4f4c3782721ce97d76289fff19539be
dungngduy <148855343+dungngduy@users.noreply.github.com>
2023-11-08 17:17:26 +0700

    update subject details

--- Website_hotroSV/main.js
+++ Website_hotroSV/main.js
@@ -1,13 +1,35 @@
-// $('#menu > li').hover(function() {
-//     // khi thẻ menu li bị hover thì drop down menu thuộc thẻ li đó sẽ trượt xuống(hiện)
-//     $('.dropdown', this).slideDown();
-// },function() {
-//     // khi thẻ menu li bị out không hover nữa thì drop down menu thuộc thẻ li đó sẽ trượt lên(ẩn)
-//     $('.dropdown', this).slideUp();
-// });
+// Lấy tất cả các phần tử có class "dropdown"
+const dropdowns = document.querySelectorAll('.dropdown');
 
-$('.dropdown > a').click(function() {
-    $(this).parent().siblings().find('div').fadeOut('500');
-    $(this).next('div').stop(true, false, true).fadeToggle('500');
-    return false;
+// Duyệt qua từng phần tử và thêm sự kiện click cho mỗi phần tử
+dropdowns.forEach(function(dropdown) {
+  dropdown.addEventListener('click', function() {
+    dropdown.classList.toggle('active');
+  });
 });
+
+// Cố định frame đăng kí ngay
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(this).scrollTop()){
+            $('.form_register').addClass('sticky');
+        }
+    });
+});
+
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var form_register = document.getElementById("form_register");
+
+// Get the offset position of the navbar
+var sticky = form_register.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    form_register.classList.add("sticky")
+  } else {
+    form_register.classList.remove("sticky");
+  }
+}
