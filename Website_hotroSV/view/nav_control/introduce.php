ae9435973e18d1866823148759956115a03c2acf
geminasnh <146547280+geminasnh@users.noreply.github.com>
2023-11-26 12:17:25 +0700

    u

--- Website_hotroSV/view/nav_control/introduce.php
+++ Website_hotroSV/view/nav_control/introduce.php
@@ -246,7 +246,30 @@
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-xl-4 col-lg-6 col-md-6">
+                    <?php 
+                           foreach($list_tintuc as $tintuc){
+                            extract($tintuc);
+                            $img_pro = $img_path.$img;
+                           echo "<div class='col-xl-4 col-lg-6 col-md-6'>
+                           <div class='h2_blog-item mb-30'>
+                               <div class='h2_blog-img'>
+                                   <a href='blog-details.html'><img src='".$img_pro."' alt=''></a>
+                               </div>
+                               <div class='h2_blog-content'>
+                                   <div class='h2_blog-content-meta'>
+                                       <span><i class='fa-thin fa-user'></i>Admin</span>
+                                       <span><i class='fa-thin fa-clock'></i>".$ngaydang."</span>
+                                   </div>
+                                   <h5 class='h2_blog-content-title'><a href='blog-details.html'>".$tieude."</a></h5>
+                                   <a href='#' class='theme-btn blog-btn t-theme-btn'>Đọc Thêm</a>
+                               </div>
+                           </div>
+                       </div>";
+                         
+                      
+                        }
+                    ?>
+                    <!-- <div class="col-xl-4 col-lg-6 col-md-6">
                         <div class="h2_blog-item mb-30">
                             <div class="h2_blog-img">
                                 <a href="blog-details.html"><img src="assets/img/blog/2/blog-1.jpg" alt=""></a>
@@ -261,8 +284,8 @@
                                 <a href="#" class="theme-btn blog-btn t-theme-btn">Đọc Thêm</a>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-xl-4 col-lg-6 col-md-6">
+                    </div> -->
+                    <!-- <div class="col-xl-4 col-lg-6 col-md-6">
                         <div class="h2_blog-item mb-30">
                             <div class="h2_blog-img">
                                 <a href="blog-details.html"><img src="assets/img/blog/2/blog-2.jpg" alt=""></a>
@@ -277,8 +300,8 @@
                                 <a href="#" class="theme-btn blog-btn t-theme-btn">Đọc Thêm</a>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-xl-4 col-lg-6 col-md-6">
+                    </div> -->
+                    <!-- <div class="col-xl-4 col-lg-6 col-md-6">
                         <div class="h2_blog-item mb-30">
                             <div class="h2_blog-img">
                                 <a href="blog-details.html"><img src="assets/img/blog/2/blog-3.jpg" alt=""></a>
@@ -293,7 +316,7 @@
                                 <a href="#" class="theme-btn blog-btn t-theme-btn">Đọc Thêm</a>
                             </div>
                         </div>
-                    </div>
+                    </div> -->
                 </div>
             </div>
         </section>
